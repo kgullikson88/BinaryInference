dc2a9327d656a66429102e7292db11fa1ae26c03
Kevin Gullikson <kevin.gullikson@gmail.com>
2015-11-02 20:19:30 -0600

    script makes a representative sample now.

--- make_orbit_fits.py
+++ make_orbit_fits.py
@@ -12,16 +12,16 @@ MIN_MASS = 2.0  # Minimum primary mass
 MAX_MASS = 20.0  # maximum primary mass
 N_SAMPLE = 500
 
-OUTPUT_BASE = 'malmquist_pool'
-OUTFILENAME = 'Simulation_Data_Malmquist.h5'
+OUTPUT_BASE = 'representative_pool'
+OUTFILENAME = 'Simulation_Data_Representative.h5'
 
 if __name__ == '__main__':
     # Make the sample
     np.random.seed(1)
-    sample_parameters = ForwardModeling.make_malmquist_sample(GAMMA, MU, SIGMA, ETA,
-                                                              size=N_SAMPLE,
-                                                              min_mass=MIN_MASS,
-                                                              max_mass=MAX_MASS)
+    sample_parameters = ForwardModeling.make_representative_sample(GAMMA, MU, SIGMA, ETA,
+                                                                   size=N_SAMPLE,
+                                                                   min_mass=MIN_MASS,
+                                                                   max_mass=MAX_MASS)
     sample_parameters['alpha'] = np.random.normal(loc=30, scale=5, size=N_SAMPLE)
     sample_parameters['beta'] = np.random.normal(loc=0.1, scale=0.03, size=N_SAMPLE)
 
