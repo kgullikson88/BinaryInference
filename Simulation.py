4eb643d331a3f5540e1ef2769d7966836d7c86e1
kgullikson88 <kevin.gullikson@gmail.com>
2015-11-06 10:29:59 -0600

    Set bins based on the low/high keywords.

--- Simulation.py
+++ Simulation.py
@@ -22,7 +22,8 @@ def make_comparison_plot(true_vals, fitted_vals, low=0, high=1, axes=None):
     axes[0].set_xlabel('True value')
     axes[0].set_ylabel('Fitted value')
 
-    _, bins, _ = axes[1].hist(true_vals, bins=10, cumulative=True, histtype='step', normed=True)
+    bins = np.linspace(low, high, 10)
+    _, bins, _ = axes[1].hist(true_vals, bins=bins, cumulative=True, histtype='step', normed=True)
     axes[1].hist(fitted_vals, bins=bins, cumulative=True, histtype='step', normed=True)
 
     ks_stat, p_value = ks_2samp(true_vals, fitted_vals)
