9f79b0c502a4e3e16d0189516f8db0d5a2558a00
kgullikson <kevin.gullikson@gmail.com>
2015-12-24 12:08:18 -0600

    Removed reveal prefix.

--- open_slideshow.sh
+++ open_slideshow.sh
@@ -1,2 +1,3 @@
 #/bin/bash
-ipython nbconvert TexMex.ipynb --to slides --post serve --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+#ipython nbconvert TexMex.ipynb --to slides --post serve --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+ipython nbconvert TexMex.ipynb --to slides --post serve
