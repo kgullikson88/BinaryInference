473f9864520ab6f72695d3a4a70687740704817c
kgullikson88 <kevin.gullikson@gmail.com>
2016-01-14 09:12:02 -0600

    Added script to fit primary star parameters with a padova grid.

---
+++ fit_parameters.py
@@ -0,0 +1,44 @@
+import pymultinest
+from isochrones import StarModel
+from isochrones.padova import Padova_Isochrone
+import sys
+import pandas as pd
+import os
+import numpy as np
+import h5py
+
+HDF5_OUTPUT = 'Primary_Parameters.h5'
+
+class MyModel(StarModel):
+    def _make_samples(self):
+        chain = np.loadtxt('{}post_equal_weights.dat'.format(self._mnest_basename))
+
+        mass = chain[:,0]
+        age = chain[:,1]
+        feh = chain[:,2]
+        distance = chain[:,3]
+        AV = chain[:,4]
+        lnprob = chain[:,-1]
+
+        df = pd.DataFrame(data=dict(mass=mass, age=age, feh=feh, distance=distance, AV=AV, lnprob=lnprob))
+        self._samples = df.copy()
+
+
+if __name__ == '__main__':
+    pad = Padova_Isochrone()
+
+    i = int(sys.argv[1])
+    pars = pd.read_csv('Primary_Parameters.csv').loc[i]
+    model = MyModel(pad, Teff=(pars.teff, pars.teff_err),
+                      logg=(pars.logg, pars.logg_err),
+                      feh=(pars.feh, pars.feh_err))
+    starname = pars.star.replace(' ', '_')
+    model.fit(basename=os.path.join('ParFitter', starname),
+              overwrite=False)
+    print(model.samples.describe())
+
+    model.samples.to_hdf(HDF5_OUTPUT, key=starname)
+    #with h5py.File(HDF5_OUTPUT, 'a') as outfile:
+    #    ds = outfile.create_dataset(starname, data=model.samples)
+    #    ds.attrs['star'] = pars.star 
+    #    ds.attrs['columns'] = model.samples.columns
