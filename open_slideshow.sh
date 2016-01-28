a4c2e0792672dd27dccc24e543e49bfbc05be14f
kgullikson88 <kevin.gullikson@gmail.com>
2016-01-28 16:37:55 -0600

    Removed TexMex script.

--- open_slideshow.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-#ipython nbconvert TexMex.ipynb --to slides --post serve --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
-ipython nbconvert TexMex.ipynb --to slides --post serve
